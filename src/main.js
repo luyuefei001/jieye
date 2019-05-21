c1d8ab23e672c75cedb8c70ead9d27ee436f7fb1
luyuefei001 <1404411437@qq.com>
2019-05-21 15:21:07 +0800

    初始化成功

--- src/main.js
+++ src/main.js
@@ -3,9 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+/**
+ * 解决移动端300毫秒点击延迟
+ * 步骤 终端输入 npm install fastclick --save 安装fastclick
+ * 重启 npm  终端输入 npm run start
+ */
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+// 1像素边框
+import './assets/styles/border.css'
+
 
 Vue.config.productionTip = false
-
+//使用fastClick 
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
